a1d24927e745da4f72dbfd8032faf4548399f064
Mlfencer <bigmacgyver01@gmail.com>
2017-03-15 21:30:43 -0400

    Edit Personal Info Layout created, needs code now

--- app/src/main/java/com/bigmacdev/all_med/MenuActivity.java
+++ app/src/main/java/com/bigmacdev/all_med/MenuActivity.java
@@ -1,13 +1,31 @@
 package com.bigmacdev.all_med;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private Button personalInfo;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        personalInfo = (Button)findViewById(R.id.personalInfoBtn);
+
+        personalInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MenuActivity.this, editPersonalInfo.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
